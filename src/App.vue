ec3873c5651f76c56155b279994878a947a032d6
umeduck <ume_duck@outlook.jp>
2023-08-21 21:57:35 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { computed } from 'vue';
+  const answer = computed(() => {
+    return 321434253451 % 12132
+  })
+</script>
+<template>
+  <h1>Vue 3</h1>
+  <p>1 + 1 = {{ answer }}</p>
+</template>
